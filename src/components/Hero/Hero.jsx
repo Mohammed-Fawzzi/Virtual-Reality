650752d9346b414944bfd8714e8672135ca19413
Moahmed Fawzzi <mohamedfawzzimohamed@gmail.com>
2026-08-03 20:09:46 +0300

    Remove custom webpack configuration and update video source paths in Hero component to use public directory.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,3 @@
-import video1 from "@/assets/video1.mp4";
-import video2 from "@/assets/video2.mp4";
 import Link from "next/link";
 
 export default function Hero() {
@@ -34,18 +32,20 @@ export default function Hero() {
             autoPlay
             loop
             muted
+            playsInline
             className="w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
           >
-            <source src={video1} type="video/mp4" />
+            <source src="/video1.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           <video
             autoPlay
             loop
             muted
+            playsInline
             className="w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
           >
-            <source src={video2} type="video/mp4" />
+            <source src="/video2.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
